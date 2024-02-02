2c523db18930e06f9cb375cde7e8db920104e9b2
Edgar Ramírez-Mondragón <edgarrm358@gmail.com>
2024-02-01 21:38:55 -0600

    chore: Simplify dev workflow

--- src/backports/httpmethod/__init__.py
+++ src/backports/httpmethod/__init__.py
@@ -1,4 +1,9 @@
-from backports.strenum import StrEnum
+import sys
+
+if sys.version_info < (3, 11):
+    from backports.strenum import StrEnum
+else:
+    from enum import StrEnum
 
 __all__ = ["HTTPMethod"]
 
