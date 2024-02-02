809a03a8b5254f416732423523a9dc7515284e10
Edgar Ramírez Mondragón <16805946+edgarrmondragon@users.noreply.github.com>
2024-02-01 21:48:14 -0600

    chore: Remove dev dependency on pytest and use backports.strenum only on < 3.11 (#18)

--- tests/test_httpmethod.py
+++ tests/test_httpmethod.py
@@ -1,27 +1,37 @@
+import sys
+import unittest
+
 from backports.httpmethod import HTTPMethod
 
 
-def test_equals_string():
-    assert HTTPMethod.GET == "GET"
+class TestHTTPMethod(unittest.TestCase):
+    def test_equals_string(self):
+        self.assertEqual(HTTPMethod.GET, "GET")
 
+    def test_value(self):
+        self.assertEqual(HTTPMethod.GET.value, "GET")
 
-def test_value():
-    assert HTTPMethod.GET.value == "GET"
+    def test_description(self):
+        self.assertEqual(HTTPMethod.GET.description, "Retrieve the target.")
 
+    def test_available_methods(self):
+        self.assertListEqual(
+            list(HTTPMethod),
+            [
+                HTTPMethod.CONNECT,
+                HTTPMethod.DELETE,
+                HTTPMethod.GET,
+                HTTPMethod.HEAD,
+                HTTPMethod.OPTIONS,
+                HTTPMethod.PATCH,
+                HTTPMethod.POST,
+                HTTPMethod.PUT,
+                HTTPMethod.TRACE,
+            ],
+        )
 
-def test_description():
-    assert HTTPMethod.GET.description == "Retrieve the target."
+    @unittest.skipIf(sys.version_info < (3, 11), "requires Python 3.11+")
+    def test_equality(self):
+        from http import HTTPMethod as PyHTTPMethod  # noqa: PLC0415
 
-
-def test_available_methods():
-    assert list(HTTPMethod) == [
-        HTTPMethod.CONNECT,
-        HTTPMethod.DELETE,
-        HTTPMethod.GET,
-        HTTPMethod.HEAD,
-        HTTPMethod.OPTIONS,
-        HTTPMethod.PATCH,
-        HTTPMethod.POST,
-        HTTPMethod.PUT,
-        HTTPMethod.TRACE,
-    ]
+        self.assertEqual(HTTPMethod.__members__, PyHTTPMethod.__members__)
